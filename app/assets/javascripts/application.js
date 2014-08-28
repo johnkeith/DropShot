c3962141be95f4f2403faf8b63f6cf3e9b242286
John Keith <johnkeith@outlook.com>
2014-08-27 23:11:12 -0400

    Gemfile, Bowerfile, assets loading in maninfests

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require angular/angular
+//= require angular-route/angular-route
+//= require angular-resource/angular-resource
+//= require angular-rails-templates
+//= require bootstrap-sass-official/assets/javascripts/bootstrap.js
