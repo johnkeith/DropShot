05d96e5f26585e40dd5da21b8199729359217082
John Keith <johnkeith@outlook.com>
2014-08-30 22:42:02 -0400

    psuedo coding of control flow inside of upload actions

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -4,8 +4,15 @@ class DropboxController < ApplicationController
   before_filter :authenticate_user!
 
   def upload_from_url
-    client = build_client
-    binding.pry
+    url = params[:url]
+    # send url from form to screenshot service
+      # screenshot service captures shot of site, saves it to a file in the /tmp dir
+    # screenshot service returns a file object
+
+
+    # client = build_client
+    # client.put_file('/db_text.txt', open(Rails.root.join('tmp','db_test.txt').to_s))
+    redirect_to root_path
   end
 
   def upload_from_file
