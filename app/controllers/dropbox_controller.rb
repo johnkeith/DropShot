1c1af65daa7ed1bb0c068da9a8e253c3f520cc51
John Keith <johnkeith@outlook.com>
2014-08-30 21:53:28 -0400

    build client method on db ctrl working

--- app/controllers/dropbox_controller.rb
+++ app/controllers/dropbox_controller.rb
@@ -1,11 +1,19 @@
+require 'dropbox_sdk'
+
 class DropboxController < ApplicationController
   before_filter :authenticate_user!
 
-  def upload
+  def upload_from_url
+    client = build_client
+    binding.pry
+  end
+
+  def upload_from_file
   end
 
   private
 
-  def build_client 
+  def build_client
+    DropboxClient.new(current_user.access_token) 
   end
 end
