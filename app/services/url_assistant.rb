680068720123c51834a092c0892955b96d96a88d
John Keith <johnkeith@outlook.com>
2014-08-31 08:50:22 -0400

    seperate service modules into different files

---
+++ app/services/url_assistant.rb
@@ -0,0 +1,13 @@
+module Services
+  class url_assistant
+    def valid_url?(url)
+      # if url doesn't return a 200, 
+        # attempt repairs
+      # else
+        # return url
+    end
+
+    def attempt_repairs(url)
+    end
+  end
+end
