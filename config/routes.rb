44628978102cd6da5c34cc5b9d74b0e8fbd86241
John Keith <johnkeith@outlook.com>
2014-08-31 21:53:58 -0400

    comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: "static_pages#index"
 
+  # for auth
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
 
+  # for dropbox
   post '/upload_from_url', to: 'dropbox#upload_from_url'
   post '/upload_from_file', to: 'dropbox#upload_from_file'
 end
