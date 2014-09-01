40e08634d4de4007c5a7f27eaad26e3de09cd2e5
John Keith <johnkeith@outlook.com>
2014-09-01 12:56:01 -0400

    reupload to fix phantomjs

--- app/services/screenshot.rb
+++ app/services/screenshot.rb
@@ -3,7 +3,7 @@ module Services
     def from_url(url, opts={})
       img_file = Rails.root.join("tmp","screenshots","screen.png").to_s
       img_file.slice!(0)
-
+      # edit to reupload to heroku since phantomjs disappeared
       width = opts.fetch(:width, 1024)
       height = opts.fetch(:height, 768)
       timeout = opts.fetch(:timeout, 10)
