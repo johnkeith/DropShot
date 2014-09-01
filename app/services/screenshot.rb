af3673f15f6fa52571ada0cab3e705baaab944da
John Keith <johnkeith@outlook.com>
2014-09-01 11:20:36 -0400

    empty tmp and tmp/screenshots dirs in app folder

--- app/services/screenshot.rb
+++ app/services/screenshot.rb
@@ -2,7 +2,7 @@ module Services
   class Screenshot
     def from_url(url, opts={})
       file = Rails.root.join("tmp","screenshots","screen.png").to_s
-      
+
       width = opts.fetch(:width, 1024)
       height = opts.fetch(:height, 768)
       timeout = opts.fetch(:timeout, 10)
